be15abde381addcb9bf8e6bc2d190f3b6d708f73
AdityaKulkarni <kulkarniaditya1997@gmail.com>
2024-04-12 17:57:25 -0700

    Removed hardcoded login

--- login/mainwindow.cpp
+++ login/mainwindow.cpp
@@ -32,12 +32,11 @@ void MainWindow::showMainWindow(){
 
 void MainWindow::on_pushButton_clicked()
 {
-    /*if(loginDataModel.getUsername().isEmpty() || loginDataModel.getPassword().isEmpty()){
+    if(loginDataModel.getUsername().isEmpty() || loginDataModel.getPassword().isEmpty()){
         QMessageBox::critical(this, "Error", "Username or password cannot be empty");
         return;
-    }else{*/
-        //bool loginSuccess = loginController.login(loginDataModel.getUsername(), loginDataModel.getPassword());
-        bool loginSuccess = loginController.login("shivam.hasurkar", "admin@123");
+    }else{
+        bool loginSuccess = loginController.login(loginDataModel.getUsername(), loginDataModel.getPassword());
 
         if(loginSuccess){
             adminDashboard = new AdminDashboard(this);
@@ -48,6 +47,6 @@ void MainWindow::on_pushButton_clicked()
         }else{
             QMessageBox::critical(this, "Invalid credentials", "Please enter valid username and password");
         }
-    //}
+    }
 }
 
