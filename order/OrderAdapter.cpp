53bcd615baf91932999c4d9ea25681ed057a44d0
Vijitha Gunta <vijitha.gunta@sjsu.edu>
2024-05-24 14:31:35 -0700

    save correct details

--- order/OrderAdapter.cpp
+++ order/OrderAdapter.cpp
@@ -1,60 +1,54 @@
 #include "OrderAdapter.h"
 #include <sstream>
-#include <fstream>
 #include <stdexcept>
 #include <iostream>
 #include "../menu/Menu.h"
 
-std::string OrderAdapter::serialize(const Order &order)
-{
+std::string OrderAdapter::serialize(const Order &order) {
     std::ostringstream oss;
     oss << order.getOrderID() << "," << order.getStatus();
 
-    order.forEachItem([&oss](const OrderItem &item)
-                      { oss << "," << item.getMenuItem()->getName() << ";" << item.getQuantity(); });
+    order.forEachItem([&oss](const OrderItem &item) {
+        oss << "," << item.getMenuItem()->getName() << ";"
+            << item.getMenuItem()->getPrice() << ";"
+            << item.getQuantity();
+    });
 
     return oss.str();
 }
 
-Order OrderAdapter::deserialize(const std::string &csvLine)
-{
+Order OrderAdapter::deserialize(const std::string &csvLine) {
     std::istringstream ss(csvLine);
     std::string segment;
     std::vector<std::string> seglist;
 
-    while (std::getline(ss, segment, ','))
-    {
+    while (std::getline(ss, segment, ',')) {
         seglist.push_back(segment);
     }
 
-    if (seglist.size() < 3) // Ensure at least ID, status, and one item are present
+    if (seglist.size() < 3)
         throw std::runtime_error("CSV format error");
 
     int orderID = std::stoi(seglist[0]);
     std::string status = seglist[1];
 
     Order order(orderID);
+    order.setStatus(status);
 
-    Menu menu;
-
-    for (size_t i = 3; i < seglist.size(); ++i)
-    {
+    for (size_t i = 2; i < seglist.size(); ++i) {
         std::istringstream itemStream(seglist[i]);
-        std::string itemName;
-        std::getline(itemStream, itemName, ';');
-        int quantity;
-        itemStream >> quantity;
+        std::string itemName, itemPriceStr, itemQuantityStr;
 
-        auto it = menu.findItemByName(itemName);
-        if (it != menu.getMenuItems().end())
-        {
-            OrderItem *orderItem = new OrderItem(&(*it), quantity); // &(*it) gets the address of the item pointed by iterator
-            order.addItem(orderItem);
-        }
-        else
-        {
-            std::cerr << "MenuItem not found during deserialization: " << itemName << std::endl;
-        }
+        std::getline(itemStream, itemName, ';');
+        std::getline(itemStream, itemPriceStr, ';');
+        std::getline(itemStream, itemQuantityStr, ';');
+
+        double itemPrice = std::stod(itemPriceStr);
+        int itemQuantity = std::stoi(itemQuantityStr);
+
+        MenuItem *menuItem = new MenuItem(itemName, itemPrice, "", "", Category::MainCourse, {}, Availability::Available);
+        OrderItem *orderItem = new OrderItem(menuItem, itemQuantity);
+        order.addItem(orderItem);
     }
 
     return order;
